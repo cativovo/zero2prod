61a2aaeeaf7964041999c6b5d1ab800e38fa85bf
Mark Leo Cativo <leo.cativo@coherent.global>
2023-07-28 01:54:00 +0800

    feat: add health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::Server;
+
+#[tokio::main]
+async fn main() {
+    let app = zero2prod::app();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
